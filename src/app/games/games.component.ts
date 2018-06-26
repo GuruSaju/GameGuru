27500f840acbda12009e87648a3f689968dab7b2
Srisarguru Sridhar <srisargurusridhar@u.boisestate.edu>
2018-06-25 23:10:54 -0400

    adding some mock data and using ngFor to repeat - 20180625

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,17 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import {Game} from '../game';
+import {GAMES} from '../mock-games';
+
 @Component({
   selector: 'app-games',
   templateUrl: './games.component.html',
   styleUrls: ['./games.component.css']
 })
 export class GamesComponent implements OnInit {
-  game : Game = {
-    serialNo: 1,
-    name: 'Horizon Zero Dawn',
-    review: 'Wonderful world filled with robotic animals to battle and hunt with a beautiful story',
-    rating: 9.4
-  };
+  games = GAMES;
   
   constructor() { }
 
---
+++ src/app/mock-games.ts
@@ -0,0 +1,12 @@
+import {Game} from './game';
+
+export const GAMES: Game[] = [
+    { serialNo: 1, name: 'Horizon Zero Dawn', review:'Wonderful world filled with robotic animals to battle and hunt with a beautiful story', rating : 9.4},
+    { serialNo: 2, name: 'Fifa 16', review:'Great addition to the fifa series!', rating : 8.7},
+    { serialNo: 3, name: 'Call of Duty: Black Ops 3', review:'Great multiplayer with a mediocre campaign', rating : 7.5},
+    { serialNo: 4, name: 'Ghost Recon Wildlands', review:'Huge map with lot\'s of repetitive campaigns', rating : 8.3},
+    { serialNo: 5, name: 'Batman Arkham Knight', review:'Be the Batman and feel like one', rating : 9.0},
+    { serialNo: 6, name: 'Far Cry 4', review:'Explore the world of kyrat with wildlife', rating : 9.1},
+    { serialNo: 7, name: 'Assasin\'s Creed Syndicate', review:'London at it\'s finest with historical characters', rating : 8.6},
+    { serialNo: 8, name: 'Far Cry Primal', review:'Be a primal and get transported to BC', rating : 8.9}
+]
