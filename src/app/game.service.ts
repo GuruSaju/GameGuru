18554fe59e07955534d299bff8907c09814f5bee
Srisarguru Sridhar <srisargurusridhar@u.boisestate.edu>
2018-07-01 22:23:55 -0400

    injecting game service to get data synchronously - 20180701

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+
+import { GAMES } from './mock-games';
+import { Game } from './game';
+
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
   constructor() { }
+
+  getGames(): Game[] {
+    return GAMES;
+  }
 }
--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import {Game} from '../game';
-import {GAMES} from '../mock-games';
+import { Game } from '../game';
 
+import { GameService } from '../game.service';
 @Component({
   selector: 'app-games',
   templateUrl: './games.component.html',
   styleUrls: ['./games.component.css']
 })
 export class GamesComponent implements OnInit {
-  games = GAMES;
-  selectedGame :Game;
-  constructor() { }
+  games: Game[];
+  selectedGame: Game;
+  constructor(private gameService: GameService) { }
 
   ngOnInit() {
+    this.getGames();
   }
 
   onSelect(game: Game): void {
-    this.selectedGame =game;
+    this.selectedGame = game;
+  }
+
+  getGames(): void {
+    this.games = this.gameService.getGames();
   }
 }
